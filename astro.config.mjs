017abb3d1839e18cf7977327c36b31281e1ba6ec
knutole <knutole@mapic.io>
2024-10-31 07:06:02 +0100

    more updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import devtoolBreakpoints from "astro-devtool-breakpoints";
 
-
 export default defineConfig({
   markdown: {
     drafts: true,
     shikiConfig: {
-      theme: "css-variables"
-    }
+      theme: "css-variables",
+    },
   },
   shikiConfig: {
     wrap: true,
     skipInline: false,
-    drafts: true
+    drafts: true,
   },
-  site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), mdx(), devtoolBreakpoints()]
+  site: "https://lexingtonthemes.com",
+  integrations: [tailwind(), sitemap(), mdx(), devtoolBreakpoints()],
 });
